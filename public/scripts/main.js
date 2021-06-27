621fd466d1b90a30d2027a827efe9661d363bb4f
Raul Garcia <raulfgarcia10@gmail.com>
2021-06-26 22:42:56 -0300

    Atualização do curso

---
+++ public/scripts/main.js
@@ -0,0 +1,44 @@
+import Modal from './modal.js';
+
+const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
+// Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click", handleClick)
+})
+
+// Quando o botão delete for clicado ele abre a modal
+const deleteButtons = document.querySelectorAll('.actions a.delete')
+
+deleteButtons.forEach(button => {
+
+    button.addEventListener("click", event => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+    event.preventDefault()
+    const textTitle = check ? "Marcar como lido" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector('#room-id').dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector('.modal form')
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
+
+    modalTitle.innerHTML = `${textTitle} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que você deseja ${textTitle.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${textTitle.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    // Abrir modal
+    modal.open()
+}
+
